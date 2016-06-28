98fb015fc4c81fe35f7a638445eaf202ec7e1f0c
smasuda <smasuda@cta-PC.(none)>
2016-06-28 18:06:56 +0900

    add rbcp.h

---
+++ DominoSoft2/RBCP/rbcp.h
@@ -0,0 +1,16 @@
+/*************************************************
+*                                                *
+* SiTCP Remote Bus Control Protocol              *
+* Header file                                    *
+*                                                *
+* 2010/05/31 Tomohisa Uchida                     *
+*                                                *
+*************************************************/
+struct rbcp_header{
+  unsigned char type;
+  unsigned char command;
+  unsigned char id;
+  unsigned char length;
+  unsigned int address;
+};
+
